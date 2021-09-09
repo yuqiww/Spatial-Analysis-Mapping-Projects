a9b4f28fbaedd62da30ed1dd6ee0f962050312e8
Yuqi <ywang@bayareametro.gov>
2021-09-09 12:48:11 -0700

    move files around in order to run Docker script on a Windows desktop

--- Project-Documentation/Active-Transportation-Plan-Data-Development/conflation_scripts/step1_batch_match_bike_rule.sh
+++ Project-Documentation/Active-Transportation-Plan-Data-Development/conflation_scripts/step1_batch_match_bike_rule.sh
@@ -15,17 +15,16 @@
 #     docker run -it --rm -v /c/Users/ywang/Documents/GitHub/Spatial-Analysis-Mapping-Projects/Project-Documentation/Active-Transportation-Plan-Data-Development:/usr/node/ shst:latest /bin/bash
 # 
 # Then you can cd to this directory, make this script executable, and run this script:
-# cd /usr/node/scripts
+# cd /usr/node/conflation_scripts
 # chmod u+x batch_match_bike_rule.sh (skip this line if running on a Windows machine)
 # ./step1_batch_match_bike_rule.sh (if getting "/bin/bash^M: bad interpreter: No such file or directory" error, 
 #   it means this file has Windows line endings, remove them by running "sed -i -e 's/\r$//' step1_batch_match_bike_rule.sh" before this step)
 
-# for filename in ../geojson/*.geojson
 for filename in ../Data/geojson/*.geojson
 do
     name=$(basename "$filename" .geojson)
     echo ${name}
 
     echo "Matching ${name} to shst using bike routing rules"
-    # shst match ../geojson/$name.geojson --out=../shst_match/$name.out1.geojson --tile-hierarchy=8 --match-bike
+    shst match ../Data/geojson/$name.geojson --out=../Data/shst_match/$name.out1.geojson --tile-hierarchy=8 --match-bike
 done
